fca3cc4da96c521a799f93e7ffa2736dc85cb85e
Kartikey Vashisht <76244793+Kartikey-V@users.noreply.github.com>
2023-01-30 23:37:20 +0530

    Day-3 Practical Code

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const Heading = React.createElement("h1", {}, "Heading 1");
-const Para = React.createElement("h3", {}, "Hello React");
-const Container = React.createElement("div", {className:"wrapper"}, [Heading, Para])
-
-
-const AppLayout = () => {
+// const Heading = React.createElement('h1', { key: 'heading1' }, 'Heading 1');
+// const Para = React.createElement('h3', { key: 'heading3' }, 'Hello React');
+// const Container = React.createElement('div', { className: 'wrapper' }, [
+//     Heading,
+//     Para,
+// ]);
+// console.log(Heading);
+//React.createElement() => Object => HTML(DOM)
+const Title = () => (
+    <h1 id="title" key="h2">
+        Namaste Kartikey
+    </h1>
+);
+const HeaderComponent = () => {
     return (
-        <>
-            {Container}
-        </>
-    )
-}
+        <div>
+            {/* <Title /> */}
+            {Title()}
+            <h2>Namaste React Functional Component</h2>
+            <h2>This is a h2 tag</h2>
+        </div>
+    );
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<AppLayout />);
+root.render(<HeaderComponent />);
---
+++ Assignments/day-3-TheoryAssignment.md
@@ -0,0 +1,4 @@
+### Doubts
+- @parcel/transformer-babel: Error while parsing config - JSON5: invalid end of input at 1:1 : this is because .babelrc file cannnot be empty, atleast put {} in the file.
+- Console.log(React.createElement()) makes a object
+- JSX is a HTML Like sysntax, but not HTML
